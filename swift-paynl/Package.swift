c08acdcf7d5795e543129c845a33154d4dfdd35b
Zhanna Hakobyan <zh.hakobyan@tuparev.com>
2025-03-10 17:19:46 +0400

    Updated platform requirement to macOS14.

--- swift-paynl/Package.swift
+++ swift-paynl/Package.swift
@@ -1,24 +1,19 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-paynl",
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift-paynl",
-            targets: ["swift-paynl"]),
+        .library(name: "swift-paynl", targets: ["swift-paynl"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swift-paynl"),
-        .testTarget(
-            name: "swift-paynlTests",
-            dependencies: ["swift-paynl"]
-        ),
+        .target(name: "swift-paynl"),
+        .testTarget(name: "swift-paynlTests", dependencies: ["swift-paynl"]),
     ]
 )
